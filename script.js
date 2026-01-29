ccba02ffd7c60bf256ecdeb2d626088751231976
DookieMercado <davyjones3211512345@gmail.com>
2026-01-29 20:43:21 +0800

    new RESUME

--- script.js
+++ script.js
@@ -177,7 +177,7 @@ function initPdfModal() {
     const pdfCloseBtn = document.querySelector('.pdf-close-btn');
     const pdfViewer = document.getElementById('pdfViewer');
     const viewResumeBtn = document.getElementById('viewResumeBtn');
-    const pdfUrl = 'files/ResumeM12.pdf';
+    const pdfUrl = 'files/RESUMED.pdf';
 
     viewResumeBtn.addEventListener('click', e => {
         e.preventDefault();
